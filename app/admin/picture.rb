16ba95ae04d3922ad7ff61bf84b3fe3a8c42eda8
maangie <maangie906@gmail.com>
2015-10-26 20:29:54 +0900

    add "Picture" model to "ActiveAdmin".

--- app/admin/picture.rb
+++ app/admin/picture.rb
@@ -11,4 +11,44 @@ ActiveAdmin.register Picture do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+
+  permit_params :id, :name, :photo
+  
+  index do
+    column "ID", :id
+    column "name", :name
+    column "thumb", :photo do |obj|
+      image_tag(obj.photo.url(:thumb))
+    end
+    actions
+  end
+
+  form multipart: true do |f|
+    f.inputs Picture do
+      f.input :name
+      f.input :photo, as: :file, hint: image_tag(f.object.photo.url(:medium))
+
+      f.actions
+    end
+  end
+
+
+  show do |b|
+    attributes_table do
+      row :name
+      row :photo do
+        image_tag(b.photo.url(:medium))
+      end
+      row :url do
+        b.photo.url
+      end
+      row :url_medium do
+        b.photo.url(:medium)
+      end
+      row :url_thumb do
+        b.photo.url(:thumb)
+      end
+    end
+    active_admin_comments
+  end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ActiveRecord::Base
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment :photo,
+                       content_type: {
+                         content_type: ["image/jpg", "image/jpeg", "image/png",
+                                        "image/gif"]
+                       }
 end
